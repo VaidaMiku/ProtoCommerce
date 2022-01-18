70d3e3696dbf029d09a1285e94a50a3dcaddfdb8
vaida <vaida.miku@gmail.com>
2022-01-18 11:18:45 +0200

    Update driver

--- src/main/java/com/proto/base/BaseTest.java
+++ src/main/java/com/proto/base/BaseTest.java
@@ -1,15 +1,15 @@
 package com.proto.base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 import java.io.IOException;
-import java.net.URL;
 import java.time.Duration;
 
 import static com.proto.util.TestDataReader.*;
@@ -22,6 +22,12 @@ public class BaseTest {
 
     @BeforeTest
     public void setUp() throws IOException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
 
         String browser = getBrowser();
         String baseUrl = getUrl();
